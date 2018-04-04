b4aaccc0ae1e57b13f7acc4b1f6ce7d19e7da1e3
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-04 14:50:33 +0200

    Solved exercises in chapter 02

---
+++ chapter02/exercise01 - Looping a triangle/script.js
@@ -0,0 +1,10 @@
+/*
+Small program to make a triangle of
+ */
+
+let triangleString = "";
+
+for (let index = 0; index < 7; index++) {
+    triangleString += "#";
+    console.log(triangleString);
+}
