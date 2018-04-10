63a22a7f347eea94d7989d2140cc35f7c99766cd
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-10 16:34:46 +0200

    Solved exercises in chapter 02

---
+++ chapter02/exercise03 - Chess Board/script.js
@@ -0,0 +1,23 @@
+const SIZE = 16;
+
+for(let i = 1; i <= SIZE; i++) {
+    let chessRow = '';
+
+    if(i % 2 !== 0) {
+        for(let k = 1; k <= SIZE; k++) {
+            if(k % 2 !== 0)
+                chessRow += ' ';
+            else
+                chessRow += '#';
+        }
+    }
+    else {
+        for(let k = 1; k <= SIZE; k++) {
+            if(k % 2 !== 0)
+                chessRow += '#';
+            else
+                chessRow += ' ';
+        }
+    }
+    console.log(chessRow);
+}
