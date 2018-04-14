ffd74f6a39db41e00ef26782df2e1f091ac424ec
Eqliphex <tbt_paddik@hotmail.com>
2018-04-14 12:20:18 +0200

    Completed exercises chapter 04

--- chapter04 - Data Structures Objects and Arrays/exercise04-deep-comparison/exercise04-deep-comparison.js
+++ chapter04 - Data Structures Objects and Arrays/exercise04-deep-comparison/exercise04-deep-comparison.js
@@ -3,27 +3,44 @@
  */
 
 
-let a = {value: 1, value2: 2};
-let b = {value: 1, value2: 2};
-let indexValue1 = -1;
-let indexValue2 = -1;
+let a = {value: 1, nextLevel: {a: {value: 2}}};
+let b = {value: 1, nextLevel: {a: {valueDeep: 2}}};
 
 function deepEqual(value1, value2) {
-    // if((Object.keys(value1) === Object.keys(value2)));
 
-    // for(Object.keys(value1))
+    // Case if both are objects:
     if ((typeof value1 === "object" && value1 !== null) && (typeof value2 === 'object' && value2 !== null)) {
-        indexValue1++;
-        indexValue2++;
         let value1Keys = Object.keys(value1);
         let value2Keys = Object.keys(value2);
 
-        return deepEqual(value1[value1Keys[indexValue1]], value2[value2Keys[indexValue2]]);
+        if((value1Keys.length !== value2Keys.length)) return false;
+        let isEqual = true;
+        for (let i = 0; i < Object.keys(value1).length; i++) {
+            let a0 = value1[value1Keys[i]];
+            let a1 = value2[value2Keys[i]];
+            if (!(deepEqual(a0, a1)))
+                return !isEqual;
+
+        }
     } else
         return (value1 === value2);
+    return true;
 }
+
+function deepEqualJSON(a, b) {
+    return  JSON.stringify(a) === JSON.stringify(b);
+
+}
+console.time('deepEqual');
+console.log(deepEqual(a, b));;
+console.timeEnd('deepEqual');
+
+console.time('JSONEqual');
+console.log(deepEqualJSON(a, b));;
+console.timeEnd('JSONEqual');
 
 
 module.exports = {
-    deepEqual
+    deepEqual,
+    deepEqualJSON
 };
--- chapter04 - Data Structures Objects and Arrays/exercise04-deep-comparison/mocha-tester.js
+++ chapter04 - Data Structures Objects and Arrays/exercise04-deep-comparison/mocha-tester.js
@@ -12,7 +12,7 @@ describe('DeepComparison', function () {
 
     describe('two unequal variables', function () {
         it('should return false when two not equal values', function () {
-           assert.equal(deepEqual(1, 2), false, 'isFalse');
+            assert.equal(deepEqual(1, 2), false, 'isFalse');
         });
     });
 
@@ -25,6 +25,25 @@ describe('DeepComparison', function () {
     describe('value2 is null', function () {
         it('should return false when one is null', function () {
             assert.equal(deepEqual(1, null), false);
-        });
+        })
+    });
+
+    describe('one object, one null', function () {
+        it('should return false when one is null', function () {
+            let value1 = {value: 1};
+            let value2 = null;
+
+            assert.equal(deepEqual(value1, value2, null), false);
+        })
+    });
+
+    describe('two equal objects', function () {
+
+        let value1 = {data1: 1, data2: 2};
+        let value2 = {data1: 1, data2: 2};
+
+        it('should return true when both are equal', function () {
+            assert.equal(deepEqual(value1, value2), true);
+        })
     });
 });
