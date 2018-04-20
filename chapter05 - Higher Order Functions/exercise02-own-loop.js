46991418cc39fce5175751715472396e17b1997f
Eqliphex <tbt_paddik@hotmail.com>
2018-04-20 16:20:37 +0200

    Completed exercises chapter 05

---
+++ chapter05 - Higher Order Functions/exercise02-own-loop.js
@@ -0,0 +1,6 @@
+function loop(start, testFunction, updateFunction, bodyFunction) {
+    for(let value = start; testFunction(value); value = updateFunction(value)) {
+        bodyFunction(value)
+    }
+}
+loop(3, n => n > 0, n => n - 1, console.log);
